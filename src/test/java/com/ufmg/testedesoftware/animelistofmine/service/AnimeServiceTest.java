0ca85d2402d90a1c15936b30025d0d56558470b1
Felipe Guimaraes <felipeguima4@gmail.com>
2022-05-14 19:08:19 -0300

    break the build to see in actions

---
+++ src/test/java/com/ufmg/testedesoftware/animelistofmine/service/AnimeServiceTest.java
@@ -0,0 +1,13 @@
+package com.ufmg.testedesoftware.animelistofmine.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AnimeServiceTest {
+
+  @Test
+  void itShouldFailBuild() {
+    fail();
+  }
+}
