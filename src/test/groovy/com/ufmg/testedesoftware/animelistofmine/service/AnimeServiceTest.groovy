81f07fce723f0ce388bcd6b296fa69a189de3064
Felipe Guimaraes <felipeguima4@gmail.com>
2022-05-15 17:07:25 -0300

    refactor tests

--- src/test/groovy/com/ufmg/testedesoftware/animelistofmine/service/AnimeServiceTest.groovy
+++ src/test/groovy/com/ufmg/testedesoftware/animelistofmine/service/AnimeServiceTest.groovy
@@ -17,24 +17,20 @@ class AnimeServiceTest extends Specification {
 
     def "it should return all animes saved in data base successfully"(){
         given:
-        List<Anime> savedAnimes = [createAnime(), createAnime(2L, "Sailor Moon", 9.0)]
+        Anime savedAnime0 = createAnime()
+        Anime savedAnime1 = createAnime(2L, "Sailor Moon", 9.0)
 
         when:
         List<Anime> listOfAnime = animeService.listAll()
 
         then:
-        1 * animeRepository.findAll() >> savedAnimes
+        1 * animeRepository.findAll() >> [savedAnime0, savedAnime1]
 
-        and:
+        and: 'validate the method return'
         assert ObjectUtils.isNotEmpty(listOfAnime)
         assert listOfAnime.size() == 2
 
-        assert listOfAnime.get(0).getId() == 1L
-        assert listOfAnime.get(0).getName() == "Dragon Ball Z"
-        assert listOfAnime.get(0).getScore() == 9.0
-
-        assert listOfAnime.get(1).getId() == 2L
-        assert listOfAnime.get(1).getName() == "Sailor Moon"
-        assert listOfAnime.get(1).getScore() == 9.0
+        assert listOfAnime.get(0) == savedAnime0
+        assert listOfAnime.get(1) == savedAnime1
     }
 }
