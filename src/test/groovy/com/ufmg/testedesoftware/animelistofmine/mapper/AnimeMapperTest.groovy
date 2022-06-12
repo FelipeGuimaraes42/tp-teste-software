160bdaab4621307eb5cdc4465c9bd7fecf35aa51
Felipe Guimaraes <felipeguima4@gmail.com>
2022-06-11 23:32:36 -0300

    setup jacoco

--- src/test/groovy/com/ufmg/testedesoftware/animelistofmine/mapper/AnimeMapperTest.groovy
+++ src/test/groovy/com/ufmg/testedesoftware/animelistofmine/mapper/AnimeMapperTest.groovy
@@ -12,7 +12,7 @@ class AnimeMapperTest extends Specification {
         AnimePostRequestBody animePostRequestBody = new AnimePostRequestBody("ODDTAXI", 10.0)
 
         when:
-        Anime anime = AnimeMapper.INSTANCE.toAnime(animePostRequestBody)
+        Anime anime = AnimeMapper.INSTANCE.fromAnimePostRequestBodyToAnime(animePostRequestBody)
 
         then: 'verify if the conversion was a success'
         assert anime instanceof Anime
@@ -23,12 +23,24 @@ class AnimeMapperTest extends Specification {
         assert anime.getName() == animePostRequestBody.getName()
         assert anime.getScore() == animePostRequestBody.getScore()
     }
+
+    def "It should convert a AnimePostRequestBody into a Movie successfully 2"() {
+        given:
+        AnimePostRequestBody animePostRequestBody = null
+
+        when:
+        Anime anime = AnimeMapper.INSTANCE.fromAnimePostRequestBodyToAnime(animePostRequestBody)
+
+        then: 'verify if the conversion was a success'
+        assert ObjectUtils.isEmpty(anime)
+    }
+
     def "It should convert a AnimePutRequestBody into a Movie successfully"() {
         given:
         AnimePutRequestBody animePutRequestBody = new AnimePutRequestBody(1L, "ODDTAXI", 10.0)
 
         when:
-        Anime anime = AnimeMapper.INSTANCE.toAnime(animePutRequestBody)
+        Anime anime = AnimeMapper.INSTANCE.fromAnimePutRequestBodyToAnime(animePutRequestBody)
 
         then: 'verify if the conversion was a success'
         assert anime instanceof Anime
@@ -39,5 +51,16 @@ class AnimeMapperTest extends Specification {
         assert anime.getId() == animePutRequestBody.getId()
         assert anime.getName() == animePutRequestBody.getName()
         assert anime.getScore() == animePutRequestBody.getScore()
+    }
+
+    def "It should convert a AnimePutRequestBody into a Movie successfully 2"() {
+        given:
+        AnimePutRequestBody animePutRequestBody = null
+
+        when:
+        Anime anime = AnimeMapper.INSTANCE.fromAnimePutRequestBodyToAnime(animePutRequestBody)
+
+        then: 'verify if the conversion was a success'
+        assert ObjectUtils.isEmpty(anime)
     }
 }
