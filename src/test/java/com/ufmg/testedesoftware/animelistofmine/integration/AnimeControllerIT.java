eca6deec534a56599d9a0ee5425cfb40963d4c30
mathelho <matheus.coelho789@gmail.com>
2022-06-26 12:02:36 -0300

    Updating tests and writing two more

--- src/test/java/com/ufmg/testedesoftware/animelistofmine/integration/AnimeControllerIT.java
+++ src/test/java/com/ufmg/testedesoftware/animelistofmine/integration/AnimeControllerIT.java
@@ -15,6 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
@@ -51,9 +53,11 @@ class AnimeControllerIT {
   @Test
   @DisplayName("integrated test - listAll should return full list of animes in database")
   void itShouldListAllAnimeSavedInDBSuccessfully() {
-    Anime savedAnime = animeRepository.save(createAnime());
+    Anime savedAnime0 = animeRepository.save(createAnime());
+    Anime savedAnime1 = animeRepository.save(createAnime(2L, "Spy x Family", 9.0));
 
-    String expectedName = savedAnime.getName();
+    String expectedName0 = savedAnime0.getName();
+    String expectedName1 = savedAnime1.getName();
 
     List<Anime> animes =
         testRestTemplate
@@ -65,11 +69,16 @@ class AnimeControllerIT {
                 })
             .getBody();
 
-    Assertions.assertThat(animes).isNotNull().isNotEmpty().hasSize(1);
+    Assertions.assertThat(animes).isNotNull().isNotEmpty().hasSize(2);
 
-    Assertions.assertThat(animes.get(0)).isEqualTo(savedAnime);
-    Assertions.assertThat(animes.get(0).getId()).isEqualTo(savedAnime.getId());
-    Assertions.assertThat(animes.get(0).getName()).isEqualTo(expectedName);
+    Assertions.assertThat(animes.get(0)).isEqualTo(savedAnime0);
+    Assertions.assertThat(animes.get(1)).isEqualTo(savedAnime1);
+
+    Assertions.assertThat(animes.get(0).getId()).isEqualTo(savedAnime0.getId());
+    Assertions.assertThat(animes.get(1).getId()).isEqualTo(savedAnime1.getId());
+
+    Assertions.assertThat(animes.get(0).getName()).isEqualTo(expectedName0);
+    Assertions.assertThat(animes.get(1).getName()).isEqualTo(expectedName1);
   }
 
   @Test
@@ -84,5 +93,46 @@ class AnimeControllerIT {
 
     Assertions.assertThat(anime).isNotNull();
     Assertions.assertThat(anime.getId()).isNotNull().isEqualTo(expectedId);
+  }
+
+  @Test
+  @DisplayName("integrated test - finding anime by its name")
+  void itShouldFindAnimeByNameInDBSuccessfully() {
+    Anime savedAnime = animeRepository.save(createAnime(1L, "Oddtaxi", 9.5));
+
+    String expectedName = savedAnime.getName();
+    String url = String.format("/animes/find/?name=%s", expectedName);
+
+    List<Anime> animes =
+        testRestTemplate
+            .exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Anime>>() {
+                })
+            .getBody();
+
+    Assertions.assertThat(animes).isNotNull().isNotEmpty().hasSize(1);
+
+    Assertions.assertThat(animes.get(0).getName()).isEqualTo(expectedName);
+  }
+
+  @Test
+  @DisplayName("integrated test - returns an empty list if anime name is not found")
+  void itShouldNotFindAnimeInDB() {
+    String url = String.format("/animes/find/?name=%s", "Dragon Ball Super");
+
+    List<Anime> animes =
+        testRestTemplate
+            .exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Anime>>() {
+                })
+            .getBody();
+
+    Assertions.assertThat(animes).isNotNull().isEmpty();
   }
 }
